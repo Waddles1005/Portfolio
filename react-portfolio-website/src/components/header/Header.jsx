fce548292553b08c2fa6ef3c2e6b15bd985fa245
Waddles1005 <dtoreo7@gmail.com>
2022-05-19 10:05:49 -0700

    header css and jsx done

--- react-portfolio-website/src/components/header/Header.jsx
+++ react-portfolio-website/src/components/header/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import ME from '../../assets/class.jpg'
+import HeaderSocial from './HeaderSocial'
+
 
 const header = () => {
   return (
@@ -10,6 +13,13 @@ const header = () => {
         <h1>Pablo Lopez-Cordon</h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA />
+        <HeaderSocial />
+
+        <div className="me">
+        <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'> Scroll Down</a>
       </div>
     </header>
   )
