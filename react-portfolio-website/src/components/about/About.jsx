2286030ff65af917f4802632414d75ceb11faf6b
Waddles1005 <dtoreo7@gmail.com>
2022-06-20 19:43:23 -0700

    experince and portfolio added stuff

--- react-portfolio-website/src/components/about/About.jsx
+++ react-portfolio-website/src/components/about/About.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './about.css'
 import ME from '../../assets/classs.jpg'
-import {FaAward} from 'react-icons/fa'
-import {FiUsers} from 'react-icons/fi'
-import {VscFolderLibrary} from 'react-icons/vsc'
+import { FaAward } from 'react-icons/fa'
+import { FiUsers } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
   return (
@@ -21,21 +21,25 @@ const About = () => {
         <div className="about__content">
           <div className="about__cards">
             <article className='about__card'>
-              <FaAward className='about__icon'/>
+              <FaAward className='about__icon' />
               <h5>Experience</h5>
               <small>3+ Years Working</small>
             </article>
 
             <article className='about__card'>
-              <FiUsers className='about__icon'/>
+              <FiUsers className='about__icon' />
               <h5>Hobbies</h5>
-              <small><ul></ul></small>
+              <small>
+                <ul> Building Computers</ul>
+                <ul>Video Games/Manage Teams</ul>
+                <ul>Spending time with friends</ul>
+              </small>
             </article>
 
             <article className='about__card'>
-              <VscFolderLibrary className='about__icon'/>
+              <VscFolderLibrary className='about__icon' />
               <h5>Projects</h5>
-              <small>80+ Completed</small>
+              <small>20+ Completed</small>
             </article>
           </div>
 
--- react-portfolio-website/src/components/portfolio/Portfolio.jsx
+++ react-portfolio-website/src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/Swindle-Shop-logo.png'
 
 const portfolio = () => {
   return (
-    <section id='portfolio'>portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio_container'>
+        <article className='portfolio_item'>
+          <div className="portfolio_item-image">
+            
+          </div>
+          <h3> This is a portfolio item title</h3>
+          <a href='https://github.com/JustynSubrai/Road-Buddy' className='btn' target='_blank'>Github</a>
+          <a href='https://github.com/JustynSubrai/Road-Buddy' className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+        </div>
+      </section>
   )
 }
 
